ec0e06420c80f8858181c81c528a8c80abd5c9d8
Rusalka21 <gonzalesm.sergio@gmail.com>
2024-09-05 17:19:15 -0500

    actualizacion categoria

--- ecommerce-mmr/src/main/java/com/portfolio/controller/CategoriaController.java
+++ ecommerce-mmr/src/main/java/com/portfolio/controller/CategoriaController.java
@@ -3,13 +3,21 @@ package com.portfolio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portfolio.model.Categoria;
+import com.portfolio.model.Producto;
 import com.portfolio.service.CategoriaService;
 
 @RestController
@@ -22,5 +30,34 @@ public class CategoriaController {
 	@GetMapping
 	public ResponseEntity<List<Categoria>> listarCategorias(){
 		return ResponseEntity.ok(servicio.listarCategorias());
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> agregarCategoria(@Validated @RequestBody Categoria c) {
+		try {
+			Categoria nuevo = servicio.grabarCategoria(c);
+			return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>("Error al agregar la categoría: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+		}		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Categoria> actualizarCategoria(
+			@PathVariable int id,
+			@RequestBody Categoria categoria){
+		Categoria c = servicio.obtenerCategoria(id);
+		c.setNombre(categoria.getNombre());
+			
+		servicio.grabarCategoria(c);
+		
+		return new ResponseEntity<Categoria>(c, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Categoria> eliminarCategoria(@PathVariable int id) {
+		Categoria categoriaEliminada = servicio.obtenerCategoria(id);
+		servicio.borrarCategoria(id);
+		return new ResponseEntity<>(categoriaEliminada, HttpStatus.OK);
 	}
 }
--- ecommerce-mmr/src/main/java/com/portfolio/service/CategoriaService.java
+++ ecommerce-mmr/src/main/java/com/portfolio/service/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.portfolio.model.Categoria;
+import com.portfolio.model.Producto;
 import com.portfolio.repository.ICategoriaRepository;
 
 @Service
@@ -15,5 +16,18 @@ public class CategoriaService {
 	
 	public List<Categoria> listarCategorias(){
 		return repoCat.findAll();
+	}
+	
+	public Categoria grabarCategoria(Categoria c) {
+		return repoCat.save(c);
+	}
+	
+	public Categoria obtenerCategoria(int id) {
+		return repoCat.findById(id).get();
+	}
+	
+	public String borrarCategoria(int id) {
+		repoCat.deleteById(id);
+		return "Categoria Eliminada";
 	}
 }
